ece75ffa668a5d32b5a7d419f52471b8a6033371
JUANDA784 <125518696+JUANDA784@users.noreply.github.com>
2023-05-16 15:03:39 -0500

    Add files via upload

---
+++ ProyectoCorte3/Agregar.py
@@ -0,0 +1,47 @@
+import sys
+import PyQt5.QtWidgets as PyQT
+from PyQt5 import uic
+import sqlite3
+
+
+class Principal(PyQT.QMainWindow):
+    def __init__(self): 
+        super().__init__()
+        self.initGui()
+    
+    def initGui(self):
+        uic.loadUi("agregar.ui",self)
+        self.show()
+        
+        self.mas.clicked.connect(lambda: self.buscador())
+
+    def buscador(self):
+        uic.loadUi("agregar.ui",self)
+        self.show()
+
+        db = sqlite3.connect("busqueda_de_datos.s3db")
+        db.row_factory=sqlite3.Row
+        cursor=db.cursor()
+        consulta="select * from Personas WHERE apellido profesion = '" + profesion +"'"
+        cursor.execute(consulta)
+        resultado=cursor.fetchone()
+        print(list(resultado))
+        cursor.close()
+        db.close()
+        return resultado
+
+
+
+
+    def abrir(self):
+        Vista()
+        self.close()
+
+
+def main():
+    app=PyQT.QApplication([])
+    window= Principal()
+    sys.exit(app.exec_())
+
+if __name__=="__main__":
+    main()
